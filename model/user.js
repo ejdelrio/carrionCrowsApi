00c6362dbebd9ff743faced72160cde7a02add9d
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-18 18:35:57 -0800

    Wrote two factor auth methods on User model

--- model/user.js
+++ model/user.js
@@ -2,6 +2,9 @@
 
 const debug = require('debug')('ccApp: userModel');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const createError = require('http-errors');
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
@@ -15,16 +18,57 @@ const User = module.exports = mongoose.model('user', userSchema);
 
 User.prototype.encrypt = function(passWord) {
   debug('User Encrypt Method');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(passWord, 10, (err, hash) => {
+      if(err) reject(err);
+      this.passWord = hash;
+      resolve(this);
+    })
+  });
 };
 
 User.prototype.login = function(passWord) {
   debug('User Compare Method');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(passWord, this.passWord, (err, valid) => {
+      if(err) reject(err);
+      if(!valid) reject(createError(401, 'Access Denied :('));
+      resolve(this);
+    })
+  });
 };
 
 User.prototype.generateHash = function() {
   debug('User Generate Hash Method');
-}
+  let tries = 0;
+
+
+  return new Promise((resolve, reject) => {
+    _generateHash.call(this);
+
+    function _generateHash() {
+      this.hash = crypto.randomBytes(32).toString('hex');
+
+      this.save()
+      .then(() => resolve(this.hash))
+      .catch(err => {
+        if(tries > 3) reject(createError(err));
+        tries++;
+        return _generateHash.call(this);
+      })
+    }
+  
+  });
+};
 
 User.prototype.signHash = function(hash) {
   debug('User Sign Hash Method');
-}
+
+  return new Promise((resolve, reject) => {
+    this.generateToken()
+    .then(hash => resolve(jwt.sign({token: hash}, process.env.APP_SECRET)))
+    .catch( err => reject(err));
+  });
+};
